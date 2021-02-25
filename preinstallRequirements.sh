bc7f8574ddc94ce9fa08d98d72e539cca3d68b36
amzy 0 (mohammad amin azimi .k) <51489627+amzy-0@users.noreply.github.com>
2021-02-25 16:48:00 +0330

    Update preinstallRequirements.sh

--- preinstallRequirements.sh
+++ preinstallRequirements.sh
@@ -1,1 +1,1 @@
-sudo apt install gcc build-essential libgtk-3-dev zenity libvte-2.91-dev python3-tk python3-pil
+sudo apt install gcc build-essential libgtk-3-dev zenity libvte-2.91-dev python3-tk python3-pil -y 
